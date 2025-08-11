f4bf82c384d1f499e861f6339b2d9aa7b7d70581
quicklookit <terohat@gmail.com>
2025-08-11 15:06:53 +0200

    Update CorrelationHeatmap.js

--- CorrelationHeatmap.js
+++ CorrelationHeatmap.js
@@ -5,7 +5,7 @@ export default function CorrelationHeatmap() {
   const [labels, setLabels] = useState([]);
 
   useEffect(() => {
-    fetch('https://quick-look-4vs6-nb6hw4akg-teros-projects-abc29d1c.vercel.app/correlation')
+    fetch('/correlation')
       .then(res => res.json())
       .then(json => {
         setMatrix(json);
