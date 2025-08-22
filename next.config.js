2a7049ea76fb0752460aee4f176edfc5b34bb55e
quicklookit <terohat@gmail.com>
2025-08-22 11:33:30 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
-module.exports = { reactStrictMode: true }
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // IMPORTANT: do NOT use output: 'export' here, that removes API routes.
+  // If you previously had it, delete it.
+  // If you want smaller server bundles, you can use:
+  // output: 'standalone',
+};
+
+module.exports = nextConfig;
